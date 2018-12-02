9c99573809c4333f443c64db80d8426521667ea4
Hapachino <erickwbrownfield@gmail.com>
2018-12-02 11:32:42 -0800

    converted container to UI element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
+import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Crystal', age: 25, belt: 'red', id: 2 },
+      { name: 'Yoshi', age: 40, belt: 'green', id: 3 },
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome :)</p>
+        <Ninjas ninjas={ this.state.ninjas }/>
       </div>
     );
   }
