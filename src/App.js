74b811722649038425070571162f7a246482b111
Hapachino <erickwbrownfield@gmail.com>
2018-12-02 16:45:34 -0800

    added addNinja

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './addNinja';
 
 class App extends Component {
   state = {
@@ -9,12 +10,33 @@ class App extends Component {
       { name: 'Yoshi', age: 40, belt: 'green', id: 3 },
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    const ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas,
+    })
+  }
+  deleteNinja = (id) => {
+    const ninjas = this.state.ninjas.filter(ninja => ninja.id !== id);
+    this.setState({
+      ninjas: ninjas,
+    })
+  }
+  componentDidMount() {
+    console.log('component mounted');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated');
+    console.log(prevProps, prevState);
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome :)</p>
-        <Ninjas ninjas={ this.state.ninjas }/>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const Ninjas = (props) => {
-  const { ninjas } = props;
+const Ninjas = ({ ninjas, deleteNinja }) => {
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ ninja.id }>
-        <div>Name: { ninja.name }</div>
-        <div>Age: { ninja.age }</div>
-        <div>Belt: { ninja.belt }</div>
+    return ninja.age > 25 ? 
+    (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+        <button onClick={() => deleteNinja(ninja.id)}>Delete ninja</button>
       </div>
-    );
+    ) 
+    : null;
   })
   return (
     <div className="ninja-list">
